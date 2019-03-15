d9cd3146c6e8ebfaa9c49a5d45c0409b80f8955b
Electrictoast <jkisor90@gmail.com>
2019-03-15 16:33:26 -0600

    Stub main class

---
+++ src/MakeChange.java
@@ -0,0 +1,14 @@
+
+public class MakeChange {
+
+	public static void main(String[] args) {
+		/* TODO get user input for item price
+		 * 		get user input for money provided
+		 * 		check and make sure input is valid
+		 * 		check and make sure money provided is sufficient
+		 */
+		
+
+	}
+
+}
